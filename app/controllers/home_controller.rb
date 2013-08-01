268c5761ee63634b95352b892fd0b17c7d655f19
James Lawrence <james@kukee.co.uk>
2013-08-01 16:21:31 +0100

    Push to diagnose leap second bug (if it works on rpi, then I'll assume it;s a consistency problem between windows and ruby epoch handling)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
   def index
     @charts = [
-        ['Past 24 hours', 30.minutes, 24.hours.ago],
-        ['Past 7 days', 3.hours, 7.days.ago],
-        ['Past month', 12.hours, 1.month.ago],
-        ['Past year', 6.days, 1.year.ago]
+        ['Past 24 hours', 30.minutes, 24.hours.ago.getutc],
+        ['Past 7 days', 3.hours, 7.days.ago.beginning_of_day.getutc],
+        ['Past month', 12.hours, 1.month.ago.beginning_of_day.getutc],
+        ['Past year', 6.days, 1.year.ago.beginning_of_day.getutc]
     ]
   end
 
   def chart
-    @start = DateTime.parse(params[:start])
+    @start = DateTime.parse(params[:start]).utc
     @interval = params[:interval].to_i
     @title = params[:title]
     render layout: false
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,23 +1,22 @@
 module HomeHelper
   def get_data(start, interval)
     # Select data that fits our range
-    data = DataPoint.where(created_at: start...DateTime.now).limit(nil).sort_by(&:created_at)
+    data = DataPoint.where(created_at: start...DateTime.now.getutc).sort_by(&:created_at)
     interval = interval.to_i
     startmin = start.to_i
 
     # Group the data by our interval
     grouped = data.group_by do |dp|
-      utime = dp.created_at.to_i
-      utime -= startmin
-      divd = utime / interval
-      divd
+      utime = dp.created_at.to_i - startmin
+      utime / interval
     end
 
     # Average and return as time, value pairs
     grouped.map do |idx, bucket|
       sum = bucket.map(&:value).reduce(:+)
       avg = sum / bucket.length
-      [Time.at(startmin + idx * interval).to_datetime, avg]
+      time = Time.at(startmin + idx * interval)
+      [time, avg]
     end
   end
 end
