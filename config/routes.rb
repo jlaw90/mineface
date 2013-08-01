5aebd1f838866de5ecbe55d7bcb699f383185426
James Lawrence <james@kukee.co.uk>
2013-08-01 10:26:01 +0100

    Changed chart to a get request (ARM nginx+passenger doesn't like POSTs?)

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ PiMiner::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
-  post 'chart', to: 'home#chart'
+  get 'chart', to: 'home#chart'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
